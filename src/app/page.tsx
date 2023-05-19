c68ae5d82e83799b4655c628179f4ee9914e737b
guivicentep <guivicente.gvp@hotmail.com>
2023-05-18 22:12:19 -0300

    feat: Finalizando base do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from '@/components/Button'
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1>Hello World
-      <Button title="Gui"/>
-      <Button title="Denise"/>
-      <Button title="Ale"/>
-      <Button title="Maria"/>
-    </h1>
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
   )
 }
